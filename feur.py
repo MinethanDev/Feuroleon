02026156ee25e90ba283435c018cf5267eaddf54
Ethan <125014979+MinethanDev@users.noreply.github.com>
2023-06-09 22:25:55 +0200

    Update feur.py
    Added "quoicoubeh" and "ça va" function

--- feur.py
+++ feur.py
@@ -3,6 +3,7 @@ import discord
 from discord import File
 from discord.ext import commands
 from easy_pil import Editor, load_image_async
+from random import randint
 
 #--- Création du bot
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), description="Quoi ? Feur.")
@@ -25,22 +26,25 @@ def search_quoi(message):
     repondre = False
     for i in range(0, len(quoilist)):
         if messageup[-(len(quoilist[i])):] == quoilist[i]:
-            print("IL A DIT QUOI !")
+            print("Présence de quoi détectée")
             repondre = True
             break
-
 
 #--- Init
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='son coiffeur.'))
+    print(bot.user.id)
     print("Je suis prêt à Feuriser !") # Le bot est correctement initialisé
 
 @bot.event
 async def on_message(message):
+    msg = str(message.content)
+
+    #--- Fonction QUOI FEUR
     search_quoi(message)
     if repondre == True:
-        quelquoi = randint(1,4)
+        quelquoi = randint(1,5)
 
         if quelquoi == 1:
             background = Editor("image.png")
@@ -66,7 +70,24 @@ async def on_message(message):
             await message.reply("**-drilatère !**", mention_author=True, file=discord.File("drilatere.png"))
             print("La personne a correctement été quadrilatèrisée.")
 
+        if quelquoi == 5:
+            await message.reply("**-rterback !**", mention_author=True, file=discord.File("rterback.png"))
+            print("La personne a correctement été quarterbackisée.")
+
         repondre == False
 
+    #--- Fonction Ca va le ...
+    if str(message.author.id) != "1022158126818001017":
+        if "CA VA" in msg.upper() or "ÇA VA" in msg.upper() or "CA VA?" in msg.upper() or "ÇA VA?" in msg.upper() or "CA VA ?" in msg.upper() or "ÇA VA ?" in msg.upper():
+            auteur = str(message.author)
+            await message.reply("Ca va et toi le " + auteur[0].upper(), mention_author=True)
+            print("Ca va envoyé")
+    
+    #--- Fonction anti quoicoubeh
+    if "QUOICOUBEH" in msg.upper():
+        print("Quoicoubeh trouvé")
+        await message.reply("**Tu es cringe**", mention_author=True, file=discord.File("cringe.gif"))
+        print("Réponse au quoicoubeh envoyée")
+
 #--- Login au compte du bot
-bot.run()
+bot.run("")
